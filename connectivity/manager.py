250616e7362ae0996d8ff97a1ed0e53ca2f31ff2
lubusax <lu.bu.sax@gmail.com>
2021-02-03 12:36:11 +0000

    auto

--- connectivity/manager.py
+++ connectivity/manager.py
@@ -35,76 +35,66 @@ def is_wlan0_up():
         loggerDEBUGdim(f"wlan0 is NOT up")
         return False   
 
-# def isDeviceConnected():
-#     result = subprocess.check_output(
-#         "nmcli dev status | grep 'connected' ",
-#         shell=True).decode("utf-8")
-#     if result:
-#         loggerDEBUGdim("connected to internet (nmcli dev status)")
-#         return True
-#     else:
-#         loggerDEBUGdim("not connected to internet (nmcli dev status)")
-#         return False
-
-# def isWiFiDefined():
-#     return not fl.isDirectoryEmpty(co.DIR_WIFI_CONNECTIONS)
-
-    
-# def ensureConnectivity():
-#     if isDeviceConnected():
-#         loggerDEBUGdim(f"RAS is connected")
-#         if wificonnectProcess and wificonnectProcess.is_alive():
-#             loggerDEBUGdim(f"terminating wifi-connect {wificonnectProcess}")
-#             wificonnectProcess.terminate()        
-#         #setFlagToEthernetOrWiFi() # if Ethernet and WiFi are both available, Flag is Ethernet
-#     else:
-#         loggerDEBUGdim(f"RAS is NOT connected")
-#         loggerDEBUGdim(f"starting wifi-connect {wificonnectProcess}")
-#         if not wificonnectProcess:
-#             wificonnectProcess = Process(name="wifi-connect", target=launcher, args=("common.wificonnect",))            
-#         if not wificonnectProcess.is_alive():
-#             wificonnectProcess.start()
-
-#     time.sleep(co.PERIOD_CONNECTIVITY_MANAGER)
 def on_terminate(proc):
-    loggerDEBUGdim(f"process {proc} terminated with exit code {proc.returncode}")
+    loggerDEBUGdim( \
+        f"process {proc} terminated with exit code {proc.returncode}")
 
 def main():
-    wificonnectProcess = None
-    while True:
-        if not isPingable("1.1.1.1"):
-            if not is_eth0_up():
-                if not is_wlan0_up():
-                    loggerDEBUGdim(f"eth0 and wlan0 are both down - RAS is NOT connected")
-                    if not wificonnectProcess:
-                        wificonnectProcess = Process(   name="wifi-connect",           \
-                                                        target=launcher,               \
-                                                        args=("common.wificonnect",)  )
-                    loggerDEBUGdim(f"starting wifi-connect {wificonnectProcess}")
-                    if not wificonnectProcess.is_alive():
-                        wificonnectProcess.start()
-            else:
-                loggerDEBUGdim(f"eth0 is up but internet (1.1.1.1) can not be reached- Check the Ethernet Internet Provider. RAS is NOT connected")
-        elif wificonnectProcess:
-            if wificonnectProcess.exitcode is None:
-                loggerDEBUGdim(f"Internet (1.1.1.1) can be reached and wifi-connect is still running. Terminating wifi-connect {wificonnectProcess}")
-                loggerDEBUGdim(f"wifi-connect Process PID is {wificonnectProcess.pid}")
-                proc0 = psutil.Process(pid=wificonnectProcess.pid)
-                procs = proc0.children(recursive=True)
-                procs.insert(0, proc0)
-                for p in procs:
-                    loggerDEBUGdim(f"terminating PID {p.pid}")
-                    p.terminate()
-                gone, alive = psutil.wait_procs(procs, timeout=10, callback=on_terminate)
-                for p in alive:
-                    loggerDEBUGdim(f"killing PID {p.pid}")
-                    p.kill()
-                #os.kill(wificonnectProcess.pid, signal.SIGTERM)
-                wificonnectProcess = None      
 
-        # TODO setFlagToEthernetOrWiFi() # if Ethernet and WiFi are both available, Flag is Ethernet
+    wificonnectProcess = None
+
+    def startWifiConnect():
+
+        loggerDEBUGdim(f"eth0 and wlan0 are both down" + \ 
+                        "- RAS is NOT connected")
+
+        if not wificonnectProcess:
+            wificonnectProcess = Process(   name="wifi-connect",           \
+                                            target=launcher,               \
+                                            args=("common.wificonnect",)  )
+
+        loggerDEBUGdim(f"starting wifi-connect {wificonnectProcess}")
+
+        if not wificonnectProcess.is_alive():
+            wificonnectProcess.start()
+    
+    def terminateWifiConnect():
+        if wificonnectProcess.exitcode is None:
+            loggerDEBUGdim(f"Internet (1.1.1.1) can be reached and wifi-connect is still running. Terminating wifi-connect {wificonnectProcess}")
+            loggerDEBUGdim(f"wifi-connect Process PID is {wificonnectProcess.pid}")
+            proc0 = psutil.Process(pid=wificonnectProcess.pid)
+            procs = proc0.children(recursive=True)
+            procs.insert(0, proc0)
+            for p in procs:
+                loggerDEBUGdim(f"terminating PID {p.pid}")
+                p.terminate()
+            gone, alive = psutil.wait_procs(procs, timeout=10, callback=on_terminate)
+            for p in alive:
+                loggerDEBUGdim(f"killing PID {p.pid}")
+                p.kill()
+            wificonnectProcess = None
+
+    def messageEth0NotWorking():
+        loggerDEBUGdim( \
+            f"eth0 is up but internet (1.1.1.1) can not be reached-"+ \ 
+             " Check the Ethernet Internet Provider. RAS is NOT connected")
+
+    def handleInternetNotReachable():
+        if not is_eth0_up():
+            if not is_wlan0_up():  # wlan0 is up 1)in mode AP (when wifi connect started) 
+                startWifiConnect() # OR 2)when connected to a SSID
+        else:
+            messageEth0NotWorking()
+
+    while True:
+
+        if not isPingable("1.1.1.1"):
+            handleInternetNotReachable()
+        elif wificonnectProcess:            # internet is working,
+            terminateWifiConnect()          # wifi connect should be terminated
 
         time.sleep(co.PERIOD_CONNECTIVITY_MANAGER)
+        # TODO setFlagToEthernetOrWiFi() # if Ethernet and WiFi are both available, Flag is Ethernet
 
 
 if __name__ == "__main__":
